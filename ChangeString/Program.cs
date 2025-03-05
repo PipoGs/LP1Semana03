168218e6633e74144ca128ec5c2ceedd83211814
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-05 17:54:08 +0000

    Added Code for exercise 2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Digite uma string: ");
+            string input = Console.ReadLine();
+
+
+            Console.Write("Digite um caráter para substituir: ");
+            char c = Console.ReadLine()[0];
+            string result = input.Replace(c, 'x');
+
+            Console.WriteLine(result);
+        }
+    }
+}
