1a09a2fdeca5e3f3733964e14da731896581760b
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-05 14:56:28 +0000

    Added Code for RighSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                if (s.Length > 3 || s.Length < 8)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
