0800f33c9efabe884035dca2abadd2a2d5a3b662
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-05 21:54:38 +0000

    Fix code for ArrayMul

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ArrayMul
 {
@@ -8,15 +9,14 @@ namespace ArrayMul
         {
             float[,] matrixA = new float[2, 2];
 
-            matrixA[0, 0] = float.Parse(args[0]);
-            matrixA[0, 0] = float.Parse(args[1]);
-            matrixA[0, 0] = float.Parse(args[2]);
-            matrixA[0, 0] = float.Parse(args[3]);
+            matrixA[0, 0] = float.Parse(args[0], CultureInfo.InvariantCulture);
+            matrixA[0, 1] = float.Parse(args[1], CultureInfo.InvariantCulture);
+            matrixA[1, 0] = float.Parse(args[2], CultureInfo.InvariantCulture);
+            matrixA[1, 1] = float.Parse(args[3], CultureInfo.InvariantCulture);
 
             float[] vectorB = new float[2];
-
-            vectorB[0] = float.Parse(args[4]);
-            vectorB[1] = float.Parse(args[5]);
+            vectorB[0] = float.Parse(args[4], CultureInfo.InvariantCulture);
+            vectorB[1] = float.Parse(args[5], CultureInfo.InvariantCulture);
 
             float[] total = new float[2];
 
@@ -32,7 +32,7 @@ namespace ArrayMul
 
             for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine(total[i]);
+                Console.WriteLine($"| {total[i]:0.00} |");
             }
         }
     }
