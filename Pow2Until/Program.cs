38be90aea0b8772deab8b4a10c100406a6392aff
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-06 16:19:16 +0000

    Added call 2x method

--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -6,7 +6,16 @@ namespace Pow2Until
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            PowersOf2Until5();
+            PowersOf2Until5();
+        }
+        private static void PowersOf2Until5()
+        {
+            for(int i=1; i<=(1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+
+            }
         }
     }
 }
