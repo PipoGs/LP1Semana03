e9451b4e2c6fe25dfefb064ab783c6335e99c1c3
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-07 17:29:53 +0000

    Added Weapons and Death Condition

---
+++ WeaponSelector/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace WeaponSelector
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main method. Do not change it!
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        private static void Main(string[] args)
+        {
+            // DO NOT CHANGE THIS METHOD!
+            EnemyType enemy = Enum.Parse<EnemyType>(args[0], true);
+            Weapons weapon = ParseWeapons(args);
+            bool dies = WeaponsKillEnemy(enemy, weapon);
+            DisplayResult(enemy, dies);
+            // DO NOT CHANGE THIS METHOD!
+        }
+
+        /// <summary>
+        /// Parse the command line arguments to get the weapon.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>The weapons.</returns>
+        private static Weapons ParseWeapons(string[] args)
+        {
+            Weapons weapons = 0;
+            foreach (string i in args)
+            {
+                if (i == "SilverBullet")
+                {
+                    weapons |= Weapons.SilverBullet;
+                }
+                else if (i == "Garlic")
+                {
+                    weapons |= Weapons.Garlic;
+                }
+                else if (i == "HolyWater")
+                {
+                    weapons |= Weapons.HolyWater;
+                }
+            }
+            return weapons;
+        }
+
+        /// <summary>
+        /// Is the enemy killed with any of the weapons?
+        /// </summary>
+        /// <param name="enemy">The enemy we're trying to kill.</param>
+        /// <param name="weapons">The weapons used to try and kill the enemy.</param>
+        /// <returns>Whether the enemy was killed or not.</returns>
+        private static bool WeaponsKillEnemy(EnemyType enemy, Weapons weapons)
+        {
+
+
+
+        }
+
+        /// <summary>
+        /// Display information on whether the enemy was killed or not.
+        /// </summary>
+        /// <param name="enemy">The enemy we're trying to kill.</param>
+        /// <param name="survives">Whether the enemy was killed or not.</param>
+        private static void DisplayResult(EnemyType enemy, bool survives)
+        {
+            if (survives)
+            {
+                Console.WriteLine($"{enemy} dies");
+            }
+            else
+            {
+                Console.WriteLine($"{enemy} survives");
+            }
+        }
+    }
+}
